ee6421490d9e1491cf59e972494b9260b0dcf0fa
Varvara-08 <58881386+Varvara-08@users.noreply.github.com>
2019-12-14 16:17:56 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,133 @@
+function plus() {
+	var num1, num2, result;
+	num1 = document.getElementById('n1').value;
+	num1 = parseInt(num1);
+	num2 = document.getElementById('n2').value;
+	num2 = parseInt(num2);
+	result = num1+num2;
+    document.getElementById('out').innerHTML = result;
+}
+
+
+function minus() {
+	var num1, num2, result;
+	num1 = document.getElementById('n1').value;
+	num1 = parseInt(num1);
+	num2 = document.getElementById('n2').value;
+	num2 = parseInt(num2);
+	result = num1-num2;
+    document.getElementById('out').innerHTML = result;
+}
+
+
+
+function umnozit() {
+	var num1, num2, result;
+	num1 = document.getElementById('n1').value;
+	num1 = parseInt(num1);
+	num2 = document.getElementById('n2').value;
+	num2 = parseInt(num2);
+	result = num1*num2;
+    document.getElementById('out').innerHTML = result;
+}
+
+
+
+function razdelit() {
+	var num1, num2, result;
+	num1 = document.getElementById('n1').value;
+	num1 = parseInt(num1);
+	num2 = document.getElementById('n2').value;
+	num2 = parseInt(num2);
+	result = num1/num2;
+    document.getElementById('out').innerHTML = result;
+}
+
+/*function korenkv() {
+	var num1, num2, result;
+	num1 = document.getElementById('n1').value;
+	num1 = parseInt(num1);
+	num2 = document.getElementById('n2').value;
+	num2 = parseInt(num2);
+	result = num1num2;
+    document.getElementById('out').innerHTML = result;
+}
+
+
+
+function korenkub() {
+	var num1, num2, result;
+	num1 = document.getElementById('n1').value;
+	num1 = parseInt(num1);
+	num2 = document.getElementById('n2').value;
+	num2 = parseInt(num2);
+	result = num1num2;
+    document.getElementById('out').innerHTML = result;
+}
+
+
+function ostatok() {
+	var num1, num2, result;
+	num1 = document.getElementById('n1').value;
+	num1 = parseInt(num1);
+	num2 = document.getElementById('n2').value;
+	num2 = parseInt(num2);
+	result = num1num2;
+    document.getElementById('out').innerHTML = result;
+}
+
+
+
+function kvadrat() {
+	var num1, num2, result;
+	num1 = document.getElementById('n1').value;
+	num1 = parseInt(num1);
+	num2 = document.getElementById('n2').value;
+	num2 = parseInt(num2);
+	result = num1num2;
+    document.getElementById('out').innerHTML = result;
+}
+
+
+function kub() {
+	var num1, num2, result;
+	num1 = document.getElementById('n1').value;
+	num1 = parseInt(num1);
+	num2 = document.getElementById('n2').value;
+	num2 = parseInt(num2);
+	result = num1num2;
+    document.getElementById('out').innerHTML = result;
+}
+
+
+
+function faktorial() {
+	var num1, num2, result;
+	num1 = document.getElementById('n1').value;
+	num1 = parseInt(num1);
+	num2 = document.getElementById('n2').value;
+	num2 = parseInt(num2);
+	result = num1num2;
+    document.getElementById('out').innerHTML = result;
+}
+
+//document.getElementById("ref").addEventListener("click",type);
+//function type() {
+//alert("script");
+//}
+//document.querySelector("#ged").addEventListener("click",point);
+//function point() {
+//alert("computer");
+//}
+//document.getElementById(elementld:"body").addEventListener(type:"click",displayDate);
+//function displayDate() {
+//}
+//function isEmail(){
+ //var str = document.getElementById("web").value;
+ //var status = document.getElementById("red");
+ //var re = /#!$%&’*+-/=?^_`{}|~@/i;
+// if (re.test(str)) status.innerHTML = "Адрес правильный";
+// else status.innerHTML = "Адрес неверный";
+// if(isEmail(str)) status.innerHTML = "Поле пустое";
+// }
+
